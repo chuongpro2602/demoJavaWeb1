70e8a36897ad189ed67831e35eec897ca6ec5c0f
ASUS <ASUS@LAPTOP-HM21LT3P>
2021-04-26 19:24:31 +0700

    demoServlet

--- HomeExcerzice/WebContent/index.jsp
+++ HomeExcerzice/WebContent/index.jsp
@@ -7,11 +7,11 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<form action="LoginController" method="post">
-	<label>UserName</label>
-	<input type="text" name="username" value=""/>
-	<label>Password</label>
-	<input type="password" name="pass" value=""/>
+	<form action="LoginController" method="post"> 
+	<label>UserName</label> <br>
+	<input type="text" name="username" value=""/> <br>
+	<label>Password</label> <br>
+	<input type="password" name="pass" value=""/> <br>
 	<input type="submit" value="Send">
 	</form>
 </body>
---
+++ HomeExcerzice/src/Controller/LoginController.java
@@ -0,0 +1,52 @@
+package Controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginController
+ */
+//@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("/index.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String pass = request.getParameter("pass");	
+		request.setAttribute("username", username);
+		request.setAttribute("pass", pass);
+		request.getRequestDispatcher("/data.jsp").forward(request, response);
+		
+		
+////		Way 2: session
+//		HttpSession session = request.getSession();
+//		session.getAttribute("username",)
+	}
+
+}
